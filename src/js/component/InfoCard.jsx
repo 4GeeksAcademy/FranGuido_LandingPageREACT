9fde0bfca805bb5541f0b4fa31a5d7af52df3535
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-07-03 22:36:24 +0000

    added card section

---
+++ src/js/component/InfoCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const cardData = {
+	image: "https://picsum.photos/200",
+	cardTitle: "Card title",
+	cardDescription:
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	button: {
+		url: "https://www.google.com/",
+		label: "Find out more!",
+	},
+};
+
+const InfoCard = () => {
+    return (
+        <div class="card" style="width: 18rem;">
+            <img src={cardData.image} class="card-img-top" alt="..."></img>
+            <div class="card-body">
+                <h5 class="card-title">{cardData.cardTitle}</h5>
+                <p class="card-text">{cardData.cardDescription}</p>
+                <a href={cardData.button.url} class="btn btn-primary">{cardData.button.label}</a>
+            </div>
+        </div>
+    )
+}
+
+export default InfoCard
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import Navbar from "./Navbar";
-import Jumbotron from "./Jumbotron";
+import {Navbar} from "./Navbar";
+import {Jumbotron} from "./Jumbotron";
+import {InfoCard} from "./InfoCard";
+import {Fatfooter} from "./Fatfooter";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import Fatfooter from "./Fatfooter";
+
 
 //create your first component
 const Home = () => {
@@ -12,19 +15,9 @@ const Home = () => {
 		<div className="container text-center">
 			<Navbar/>
 			<Jumbotron/>
+			<InfoCard/>
 			<Fatfooter/>
-			<h1 className="text-center mt-5">Bye Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			
 		</div>
 	);
 };
